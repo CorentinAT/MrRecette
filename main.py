1d41380f90f6df14e010d4244411d7330dc345e9
ALBERT Corentin <corentin.albert@etu.univ-lyon1.fr>
2022-11-09 13:18:54 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+
+url = "https://discord.com/api/webhooks/1039873910302310463/5dRCWhSTc4snYlHGL4twNhWEUQcrb2tUOTMXFwvcKbppwzdpZaG6afDVfy_dGs_7V-Jm"
+
+data = {
+    "content" : "message content",
+    "username" : "custom username"
+}

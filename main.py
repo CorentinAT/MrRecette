46113f3fe3071d1585ec306f63443eebfe463306
ALBERT Corentin <corentin.albert@etu.univ-lyon1.fr>
2022-11-09 13:36:47 +0100

    ajout main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
 
-url = "https://discord.com/api/webhooks/1039873910302310463/5dRCWhSTc4snYlHGL4twNhWEUQcrb2tUOTMXFwvcKbppwzdpZaG6afDVfy_dGs_7V-Jm"
+webhook = "https://discord.com/api/webhooks/1039873910302310463/5dRCWhSTc4snYlHGL4twNhWEUQcrb2tUOTMXFwvcKbppwzdpZaG6afDVfy_dGs_7V-Jm"
+url="http://cumometer.games"
+r = requests.get(url, allow_redirects=True)
 
-data = {
-    "content" : "message content",
-    "username" : "custom username"
+message = {
+    "content" : "test"
 }
+
+requests.post(webhook, json=message)

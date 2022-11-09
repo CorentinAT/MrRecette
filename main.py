8e1563ac9a7d0cf5ff4be67efd4b51babdc03710
ALBERT Corentin <corentin.albert@etu.univ-lyon1.fr>
2022-11-09 17:52:23 +0100

    ajout bibliotheque ssl car probleme de verification

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import requests
-from marmiton import Marmiton, RecipeNotFound
+from marmiton import Marmiton
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 webhook = "https://discord.com/api/webhooks/1039873910302310463/5dRCWhSTc4snYlHGL4twNhWEUQcrb2tUOTMXFwvcKbppwzdpZaG6afDVfy_dGs_7V-Jm"
 
+# Search :
 query_options = {
   "aqt": "boeuf bourguignon",  # Query keywords - separated by a white space
-  "dt": "platprincipal",       # Plate type : "entree", "platprincipal", "accompagnement", "amusegueule", "sauce" (optional)
-  "exp": 2,                    # Plate price : 1 -> Cheap, 2 -> Medium, 3 -> Kind of expensive (optional)
-  "dif": 2,                    # Recipe difficulty : 1 -> Very easy, 2 -> Easy, 3 -> Medium, 4 -> Advanced (optional)
-  "veg": 0,                    # Vegetarien only : 0 -> False, 1 -> True (optional)
 }
-
 query_result = Marmiton.search(query_options)
 
+print(query_result)
+
+# Get :
 recipe = query_result[0]
-main_recipe_url = recipe["url"]
+main_recipe_url = recipe['url']
 
 try:
     detailed_recipe = Marmiton.get(main_recipe_url)  # Get the details of the first returned recipe (most relevant in our case)
@@ -23,8 +25,9 @@ except RecipeNotFound as e:
     import sys
     sys.exit(0)
 
+
 message = {
-    "content" : recette
+    "content" : detailed_recipe['name']
 }
 
 requests.post(webhook, json=message)

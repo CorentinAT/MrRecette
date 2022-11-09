9d5a8f9a47810ee5e77e1f72cc2b57a86abfe845
ALBERT Corentin <corentin.albert@etu.univ-lyon1.fr>
2022-11-09 13:56:27 +0100

    premieres requette et ajout module marmiton

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import requests
+from marmiton import Marmiton, RecipeNotFound
 
 webhook = "https://discord.com/api/webhooks/1039873910302310463/5dRCWhSTc4snYlHGL4twNhWEUQcrb2tUOTMXFwvcKbppwzdpZaG6afDVfy_dGs_7V-Jm"
-url="http://cumometer.games"
-r = requests.get(url, allow_redirects=True)
+
+query_options = {
+  "aqt": "boeuf bourguignon",  # Query keywords - separated by a white space
+  "dt": "platprincipal",       # Plate type : "entree", "platprincipal", "accompagnement", "amusegueule", "sauce" (optional)
+  "exp": 2,                    # Plate price : 1 -> Cheap, 2 -> Medium, 3 -> Kind of expensive (optional)
+  "dif": 2,                    # Recipe difficulty : 1 -> Very easy, 2 -> Easy, 3 -> Medium, 4 -> Advanced (optional)
+  "veg": 0,                    # Vegetarien only : 0 -> False, 1 -> True (optional)
+}
+
+query_result = Marmiton.search(query_options)
+
+recipe = query_result[0]
+main_recipe_url = recipe["url"]
+
+try:
+    detailed_recipe = Marmiton.get(main_recipe_url)  # Get the details of the first returned recipe (most relevant in our case)
+except RecipeNotFound as e:
+    print(f"No recipe found for '{query_options['aqt']}'")
+    import sys
+    sys.exit(0)
 
 message = {
-    "content" : "test"
+    "content" : recette
 }
 
 requests.post(webhook, json=message)

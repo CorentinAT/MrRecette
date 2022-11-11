d3ecca057027f8b1ee38f3f6c0b81ce4584bf1f6
ALBERT Corentin <corentin.albert@etu.univ-lyon1.fr>
2022-11-11 19:16:06 +0100

    apres reparation du module marmiton, recuperation de la recette detaillee a partir des query_options

--- main.py
+++ main.py
@@ -8,26 +8,32 @@ webhook = "https://discord.com/api/webhooks/1039873910302310463/5dRCWhSTc4snYlHG
 
 # Search :
 query_options = {
-  "aqt": "boeuf bourguignon",  # Query keywords - separated by a white space
+  "aqt": "tomates farcies",  # Query keywords - separated by a white space
+  "dt": "platprincipal",       # Plate type : "entree", "platprincipal", "accompagnement", "amusegueule", "sauce" (optional)
+  "exp": 2,                    # Plate price : 1 -> Cheap, 2 -> Medium, 3 -> Kind of expensive (optional)
+  "dif": 2,                    # Recipe difficulty : 1 -> Very easy, 2 -> Easy, 3 -> Medium, 4 -> Advanced (optional)
+  "veg": 0,                    # Vegetarien only : 0 -> False, 1 -> True (optional)
 }
 query_result = Marmiton.search(query_options)
 
-print(query_result)
-
 # Get :
 recipe = query_result[0]
-main_recipe_url = recipe['url']
+recipe_url = recipe["url"]
+detailed_recipe = Marmiton.get(recipe_url)
 
-try:
-    detailed_recipe = Marmiton.get(main_recipe_url)  # Get the details of the first returned recipe (most relevant in our case)
-except RecipeNotFound as e:
-    print(f"No recipe found for '{query_options['aqt']}'")
-    import sys
-    sys.exit(0)
+ingredients = ""
+for element in detailed_recipe["ingredients"]:
+    ingredients = ingredients + "\n" + element
 
+etapes = ""
+
+for element in detailed_recipe["steps"]:
+    etapes = etapes + element
+
+msg = detailed_recipe["name"] + "\n**Ingrédients :**" + ingredients + "\n" + etapes
 
 message = {
-    "content" : detailed_recipe['name']
+    "content" : msg
 }
 
 requests.post(webhook, json=message)

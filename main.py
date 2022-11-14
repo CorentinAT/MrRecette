d7ff8e9b64e27e96640f8df51e58c3728c5c5ad9
ALBERT Corentin <corentin.albert@etu.univ-lyon1.fr>
2022-11-14 13:09:37 +0100

    ajout de la partie embeds pour l'affichage de la recette

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ webhook = os.getenv("webhook")
 
 # Search :
 query_options = {
-  "aqt": "pates",  # Query keywords - separated by a white space
+  "aqt": "gratin dauphinois",  # Query keywords - separated by a white space
   "dt": "platprincipal",       # Plate type : "entree", "platprincipal", "accompagnement", "amusegueule", "sauce" (optional)
-  "exp": 2,                    # Plate price : 1 -> Cheap, 2 -> Medium, 3 -> Kind of expensive (optional)
-  "dif": 2,                    # Recipe difficulty : 1 -> Very easy, 2 -> Easy, 3 -> Medium, 4 -> Advanced (optional)
+  "exp": 1,                    # Plate price : 1 -> Cheap, 2 -> Medium, 3 -> Kind of expensive (optional)
+  "dif": 1,                    # Recipe difficulty : 1 -> Very easy, 2 -> Easy, 3 -> Medium, 4 -> Advanced (optional)
   "veg": 0,                    # Vegetarien only : 0 -> False, 1 -> True (optional)
 }
 query_result = Marmiton.search(query_options)
@@ -36,10 +36,15 @@ for element in detailed_recipe["steps"]:
     etapes = etapes + "\n" + str(i) + "- " + element
     i = i + 1
 
-msg = detailed_recipe["name"] + "\n**Ingrédients :**" + ingredients + "\n" + etapes
+msg = "**Ingrédients :**" + ingredients + "\n" + etapes
+
+embed = [{
+    "description": msg,
+    "title": detailed_recipe["name"]
+}]
 
 message = {
-    "content" : msg
+    "embeds" : embed
 }
 
 requests.post(webhook, json=message)

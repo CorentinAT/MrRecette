eece75ff3f7d1a436f13f9c2c959f63f44a353e1
Corentin <32818368+CorentinAT@users.noreply.github.com>
2022-11-15 16:03:05 +0100

    module marmiton retiré pour plus de maniabilité, recherche de recette aléatoire

--- main.py
+++ main.py
@@ -2,25 +2,57 @@ import requests
 from marmiton import Marmiton #module modifié pour que les class correspondent aux mises à jour du site
 import ssl
 import os
+import urllib
 from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+from random import randint
 
 load_dotenv()
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 webhook = os.getenv("webhook")
+marmiton = "https://www.marmiton.org"
+marmiton_recherche = "https://www.marmiton.org/recettes/recherche.aspx?"
 
 # Search :
-query_options = {
-  "aqt": "gratin dauphinois",  # Query keywords - separated by a white space
+options = {
   "dt": "platprincipal",       # Plate type : "entree", "platprincipal", "accompagnement", "amusegueule", "sauce" (optional)
   "exp": 1,                    # Plate price : 1 -> Cheap, 2 -> Medium, 3 -> Kind of expensive (optional)
-  "dif": 1,                    # Recipe difficulty : 1 -> Very easy, 2 -> Easy, 3 -> Medium, 4 -> Advanced (optional)
-  "veg": 0,                    # Vegetarien only : 0 -> False, 1 -> True (optional)
+  "dif": 1,                    # Vegetarien only : 0 -> False, 1 -> True (optional)
+  "ttlt":45                    # Maximum preparation time : 15 -> -15min, 30 -> -30min, 45 -> -45min (optional)
 }
-query_result = Marmiton.search(query_options)
 
-# Get :
+options_url = urllib.parse.urlencode(options)
+recherche_url = marmiton_recherche + options_url
+resultat_html = urllib.request.urlopen(recherche_url).read()
+
+soup = BeautifulSoup(resultat_html, 'html.parser')
+
+pages = soup.find_all('a', {'class':'SHRD__sc-1ymbfjb-1 MTkAM'})
+
+no_page_aleatoire = randint(0,17)
+
+if no_page_aleatoire!=1:
+    page_url = marmiton + pages[no_page_aleatoire]['href']
+    resultat_html = urllib.request.urlopen(page_url).read()
+    soup = BeautifulSoup(resultat_html, 'html.parser')
+
+liste_recettes = soup.find_all('a', {'class':'MRTN__sc-1gofnyi-2 gACiYG'})
+
+recette_aleatoire = liste_recettes[randint(0,11)]
+
+recette = {
+    'name': recette_aleatoire.find('h4', {'class':'MRTN__sc-30rwkm-0 dJvfhM'}).get_text(),
+    'rate': recette_aleatoire.find('span', {'class':'SHRD__sc-10plygc-0 jHwZwD'}).get_text()
+}
+
+recette_url = marmiton + recette_aleatoire['href']
+print(recette_url)
+print(recette['name'])
+print(recette['rate'])
+
+"""# Get :
 recipe = query_result[0]
 recipe_url = recipe["url"]
 detailed_recipe = Marmiton.get(recipe_url)
@@ -47,4 +79,4 @@ message = {
     "embeds" : embed
 }
 
-requests.post(webhook, json=message)
+requests.post(webhook, json=message)"""
